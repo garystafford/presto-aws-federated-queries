334a1e4a347e4b007f21c3377180eda7c6d64aa4
Gary Stafford <gary.a.stafford@gmail.com>
2020-09-02 00:46:19 -0400

    Add new example

--- sql/presto_query1.sql
+++ sql/presto_query1.sql
@@ -1,5 +1,4 @@
 -- USE tpcds.sf1
-
 -- SELECT DISTINCT
 --     c_last_name,
 --     c_first_name,
@@ -17,20 +16,25 @@
 --     c_last_name,
 --     c_first_name;
 
-SELECT cd_education_status AS education,
-       cd_credit_rating    AS credit_rating,
-       cd_gender           AS gender,
-       count(cd_gender)    AS gender_count
-FROM   customer
-       LEFT JOIN customer_demographics
-              ON c_current_cdemo_sk = cd_demo_sk
-WHERE  cd_education_status IS NOT NULL
-       AND lower(cd_education_status) != 'unknown'
-       AND cd_credit_rating IS NOT NULL
-       AND lower(cd_credit_rating) != 'unknown'
-       AND cd_gender IS NOT NULL
-GROUP  BY cd_education_status,
-          cd_credit_rating,
-          cd_gender
-ORDER  BY cd_education_status,
-          cd_credit_rating;
+SELECT
+    (year (now()) - c_birth_year) AS age,
+    cd_credit_rating AS credit_rating,
+    cd_gender AS gender,
+    count(cd_gender) AS gender_count
+FROM
+    customer
+    LEFT JOIN customer_demographics ON c_current_cdemo_sk = cd_demo_sk
+WHERE
+    c_birth_year IS NOT NULL
+    AND cd_credit_rating IS NOT NULL
+    AND lower(cd_credit_rating) != 'unknown'
+    AND cd_gender IS NOT NULL
+GROUP BY
+    c_birth_year,
+    cd_credit_rating,
+    cd_gender
+ORDER BY
+    age,
+    cd_credit_rating,
+    cd_gender;
+
--- sql/presto_query1_federated.sql
+++ sql/presto_query1_federated.sql
@@ -1,19 +1,23 @@
 -- USE tpcds.sf1
+SELECT
+    (year (now()) - c_birth_year) AS age,
+    cd_credit_rating AS credit_rating,
+    cd_gender AS gender,
+    count(cd_gender) AS gender_count
+FROM
+    tpcds.sf1.customer
+    LEFT JOIN hive.default.customer_demographics ON c_current_cdemo_sk = cd_demo_sk
+WHERE
+    c_birth_year IS NOT NULL
+    AND cd_credit_rating IS NOT NULL
+    AND lower(cd_credit_rating) != 'unknown'
+    AND cd_gender IS NOT NULL
+GROUP BY
+    c_birth_year,
+    cd_credit_rating,
+    cd_gender
+ORDER BY
+    age,
+    cd_credit_rating,
+    cd_gender;
 
-SELECT cd_education_status AS education,
-       cd_credit_rating    AS credit_rating,
-       cd_gender           AS gender,
-       count(cd_gender)    AS gender_count
-FROM   tpcds.sf1.customer
-       LEFT JOIN hive.default.customer_demographics
-              ON c_current_cdemo_sk = cd_demo_sk
-WHERE  cd_education_status IS NOT NULL
-       AND lower(cd_education_status) != 'unknown'
-       AND cd_credit_rating IS NOT NULL
-       AND lower(cd_credit_rating) != 'unknown'
-       AND cd_gender IS NOT NULL
-GROUP  BY cd_education_status,
-          cd_credit_rating,
-          cd_gender
-ORDER  BY cd_education_status,
-          cd_credit_rating;
