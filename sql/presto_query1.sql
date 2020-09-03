95e007428bdbd3045d8449ae6e67aa104ae000fe
Gary Stafford <gary.a.stafford@gmail.com>
2020-09-03 19:25:30 -0400

    Update first query

--- sql/presto_query1.sql
+++ sql/presto_query1.sql
@@ -1,24 +1,35 @@
--- USE tpcds.sf1
+-- USE tpcds.sf1;
 
+WITH credit_demographics AS (
+    SELECT
+        (year (now()) - c_birth_year) AS age,
+        cd_credit_rating AS credit_rating,
+        cd_gender AS gender,
+        count(cd_gender) AS gender_count
+    FROM
+        customer
+        LEFT JOIN customer_demographics ON c_current_cdemo_sk = cd_demo_sk
+    WHERE
+        c_birth_year IS NOT NULL
+        AND cd_credit_rating IS NOT NULL
+        AND lower(cd_credit_rating) != 'unknown'
+        AND cd_gender IS NOT NULL
+    GROUP BY
+        cd_credit_rating,
+        c_birth_year,
+        cd_gender
+)
 SELECT
-    (year (now()) - c_birth_year) AS age,
-    cd_credit_rating AS credit_rating,
-    cd_gender AS gender,
-    count(cd_gender) AS gender_count
+    age,
+    credit_rating,
+    gender,
+    gender_count
 FROM
-    customer
-    LEFT JOIN customer_demographics ON c_current_cdemo_sk = cd_demo_sk
+    credit_demographics
 WHERE
-    c_birth_year IS NOT NULL
-    AND cd_credit_rating IS NOT NULL
-    AND lower(cd_credit_rating) != 'unknown'
-    AND cd_gender IS NOT NULL
-GROUP BY
-    c_birth_year,
-    cd_credit_rating,
-    cd_gender
+    age BETWEEN 21 AND 65
 ORDER BY
     age,
-    cd_credit_rating,
-    cd_gender;
+    credit_rating,
+    gender;
 
